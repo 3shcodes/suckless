7157723df360a774c88ee4a5f7e102ecdababa24
3shcodes <3shcodes@gmail.com>
2023-09-05 17:46:37 +0530

    changed fonts ig

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -11,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SF Pro Display:size=10" };
-static const char dmenufont[]       = "SF Pro Display:size=10";
+static const char *fonts[]          = { "JetBrainsMono:size=9","Roboto:size=8","Fira Code:size=8", "SF Pro Display:size=10" };
+static const char dmenufont[]       = "Fira Code:size=8";
 
 
 // custom commands
@@ -66,7 +66,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
